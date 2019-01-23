bc6ecf3885c4b5bf576727e316eaadbb1b6f6cca
John Pfeifer <johnapfeifer@yahoo.com>
2019-01-24 09:50:58 +1100

    Create InitGlobalVars.m

---
+++ Matlab/InitGlobalVars.m
@@ -0,0 +1,8 @@
+clear;
+
+global numCFD numDP clusterNode clusterTrajNode currNodeID trajData totNode h doDFD
+global distMap trajDistMatrix queryTraj
+global clusterCenterIteration clusterRSize clusterHistCenterMap histPrevCenterRSize 
+global Emax prunedNodes nodeCheckCnt leafTrajIDList Emin
+global I J lP lQ lPQ bP bQ currBestLowNodeDist prevLowestUpBnd bndTstLst
+global currNodeList nextNodeList Eapx
