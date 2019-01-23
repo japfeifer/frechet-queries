c54517c1edf584854e54b5d4bb15a110871998cb
John Pfeifer <johnapfeifer@yahoo.com>
2019-01-24 09:49:21 +1100

    Create GetPrunedNodes4.m

---
+++ Matlab/GetPrunedNodes4.m
@@ -0,0 +1,63 @@
+% used in kNNSearch
+
+function GetPrunedNodes4(cNodeID,Q,Qid,lowBnd,upBnd)
+
+    global clusterNode prunedNodes nodeCheckCnt trajData prevLowestUpBnd
+    global currBestUpNodeDist currBestLowNodeDist
+
+    nodeCheckCnt = nodeCheckCnt + 1;
+    cRad = clusterNode(cNodeID,4);
+
+    if (clusterNode(cNodeID,5) == 1) ... % at the leaf
+
+        if upBnd < currBestLowNodeDist && lowBnd > prevLowestUpBnd % this traj centre is definitely closer than all current ones
+            prunedNodes = [cNodeID lowBnd upBnd];
+            currBestLowNodeDist = lowBnd;
+            currBestUpNodeDist = upBnd;
+        elseif lowBnd <= currBestUpNodeDist && lowBnd > prevLowestUpBnd
+            prunedNodes = [prunedNodes; cNodeID lowBnd upBnd];
+            if lowBnd < currBestLowNodeDist
+                currBestLowNodeDist = lowBnd;
+            end
+            if upBnd < currBestUpNodeDist
+                currBestUpNodeDist = upBnd;
+            end
+        end
+    elseif currBestUpNodeDist >= lowBnd - cRad && ...% the node may contain a traj that we want to include
+           prevLowestUpBnd < upBnd + cRad
+
+        cID = clusterNode(cNodeID,2);
+        centerTrajID = clusterNode(cID,6);
+        % if next child centre traj = curr parent centre traj then do
+        % not have to compute it again
+        if centerTrajID == clusterNode(cNodeID,6) 
+            lowBnd1 = lowBnd;
+            upBnd1 = upBnd;
+        else
+            centreTraj = cell2mat(trajData(centerTrajID,1)); % get center traj 
+            lowBnd1 = GetBestLowerBound(centreTraj,Q,Inf,1,centerTrajID,Qid);
+            upBnd1 = GetBestUpperBound(centreTraj,Q,1,centerTrajID,Qid);
+        end
+
+        cID = clusterNode(cNodeID,3);
+        centerTrajID = clusterNode(cID,6);
+        % if next child centre traj = curr parent centre traj then do
+        % not have to compute it again
+        if centerTrajID == clusterNode(cNodeID,6)
+            lowBnd2 = lowBnd;
+            upBnd2 = upBnd;
+        else
+            centreTraj = cell2mat(trajData(centerTrajID,1)); % get center traj 
+            lowBnd2 = GetBestLowerBound(centreTraj,Q,Inf,1,centerTrajID,Qid);
+            upBnd2 = GetBestUpperBound(centreTraj,Q,1,centerTrajID,Qid);
+        end
+
+        if lowBnd1 < lowBnd2
+            GetPrunedNodes4(clusterNode(cNodeID,2),Q,Qid,lowBnd1,upBnd1);
+            GetPrunedNodes4(clusterNode(cNodeID,3),Q,Qid,lowBnd2,upBnd2);
+        else
+            GetPrunedNodes4(clusterNode(cNodeID,3),Q,Qid,lowBnd2,upBnd2);
+            GetPrunedNodes4(clusterNode(cNodeID,2),Q,Qid,lowBnd1,upBnd1);
+        end
+    end
+end
