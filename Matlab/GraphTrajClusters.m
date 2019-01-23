ef637cd633e48a4ea54d8234bf7c2813ddd36c08
John Pfeifer <johnapfeifer@yahoo.com>
2019-01-24 09:49:51 +1100

    Create GraphTrajClusters.m

---
+++ Matlab/GraphTrajClusters.m
@@ -0,0 +1,22 @@
+% graph trajectory clusters
+
+kSize = 8;
+kIndex = log2(kSize);
+
+% create some random colors for each of the k clusters
+colorList = rand(kSize,3);
+
+figure
+hold on;
+title(['Trajectory Clusters for k = ' num2str(kSize)])
+
+for i = 1:size(clusterCurveGroup,1) % plot each traj
+    currTraj = cell2mat(trajData(i,1));  % get traj
+    currCurveCluster = cell2mat(clusterCurveGroup(i,1)); % get traj cluster list
+    currClusterID = currCurveCluster(kIndex,2); % get traj cluster id for KSize value
+    currColor = colorList(currClusterID,:);  % set color for traj
+    plot(currTraj(:,1),currTraj(:,2),'linewidth',1,'color',currColor); 
+    
+end
+
+axis equal;
