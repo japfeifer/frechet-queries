ef4aa109540d70b824d6a24639506e52227dcbd3
John Pfeifer <johnapfeifer@yahoo.com>
2019-01-24 09:50:34 +1100

    Create ImportPigeonHomingData.m

---
+++ Matlab/ImportPigeonHomingData.m
@@ -0,0 +1,37 @@
+% import pigeon homing data
+
+tic;
+filename = 'PigeonHomingData.csv';
+delimiterIn = ',';
+headerlinesIn = 1;
+TBData = importdata(filename);
+
+% massage data to new format
+trajData = {[]};
+currTraj = [];
+trajCount = 1;
+dataSize = size(TBData,1);
+
+for i = 2:dataSize     
+    currRec = split(TBData(i),",");
+    currID = str2num(cell2mat(currRec(9)));
+    currX = str2num(cell2mat(currRec(4)));
+    currY = str2num(cell2mat(currRec(5)));
+    
+    if i==2 % first row   
+        prevObjectID = currID;
+        currTraj = [currX currY];
+    else
+        if (currID ~= prevObjectID)
+            trajData(trajCount,1) = mat2cell(currTraj,size(currTraj,1),size(currTraj,2));
+            trajCount = trajCount + 1;
+            currTraj = [currX currY];
+        else
+            currTraj = [currTraj; currX currY];
+        end
+        prevObjectID = currID;
+    end   
+end
+trajData(trajCount,1) = mat2cell(currTraj,size(currTraj,1),size(currTraj,2));
+
+timeElapsed = toc;
