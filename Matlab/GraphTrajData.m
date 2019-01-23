657c6f4d71d4f1eb7f1f2dfd956560c35c0393ed
John Pfeifer <johnapfeifer@yahoo.com>
2019-01-24 09:49:53 +1100

    Create GraphTrajData.m

---
+++ Matlab/GraphTrajData.m
@@ -0,0 +1,13 @@
+% create data
+P = trajData(:,:);
+% P = queryTraj(:,:);
+% P = anchorTraj;
+% P = trajData(1:1000,:);
+
+figure
+for i=1:size(P,1)
+    Q = cell2mat(P(i,1));
+    plot(Q(:,1),Q(:,2),'linewidth',1);
+    hold on;
+end
+axis equal
