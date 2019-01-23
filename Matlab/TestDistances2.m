4b5b3a2049de40f71a4639b8e682eccde8fde375
John Pfeifer <johnapfeifer@yahoo.com>
2019-01-24 09:53:39 +1100

    Create TestDistances2.m

---
+++ Matlab/TestDistances2.m
@@ -0,0 +1,32 @@
+% Test distances
+% compare continuous frechet, discrete frechet, and approx discrete frechet
+
+tic;
+PSize = size(trajData,1);
+numQuery = size(trajData,1);
+
+PSize = 100;
+numQuery = 20;
+
+totTests = PSize * numQuery;
+qcnt = 0; dist1 = 0; dist2 = 0; dist3 = 0;
+testDist = {[]};
+h = waitbar(0, 'Test Distances');
+for i = 1:numQuery % number of different queries
+
+    Q = cell2mat(trajData(i,1));
+    for j = 1:PSize
+        P = cell2mat(trajData(j,1));
+        dist1 = ContFrechet(P,Q,1);
+        
+%         LBdist = GetBestLowerBound(P,Q,Inf,2,j,i);
+%         UBdist = GetBestUpperBound(P,Q,2,j,i);
+
+        qcnt = qcnt + 1;
+    end
+    X = ['Test Distances ',num2str(qcnt),'/',num2str(totTests)];
+    waitbar(qcnt/totTests, h, X);
+end
+
+close(h);
+timeElapsed = toc;
